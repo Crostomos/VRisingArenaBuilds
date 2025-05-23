df7a8e283aac690f4bc71a7c678e8081b5e210d1
Crostomos <Crostomos@hotmail.com>
2025-05-23 14:02:05 +0200

    code cleanup

--- Core.cs
+++ Core.cs
@@ -8,17 +8,14 @@ namespace ArenaBuildsMod;
 
 internal class Core
 {
-    public static World Server { get; } = GetWorld("Server") ?? throw new Exception("There is no Server world.");
+    public static World Server => GetWorld("Server") ?? throw new Exception("There is no Server world.");
 
-    public static PrefabCollectionSystem PrefabCollectionSystem { get; } =
-        Server.GetExistingSystemManaged<PrefabCollectionSystem>();
-    
-    public static ActivateVBloodAbilitySystem ActivateVBloodAbilitySystem { get; } =
-        Server.GetExistingSystemManaged<ActivateVBloodAbilitySystem>();
+    public static PrefabCollectionSystem PrefabCollectionSystem => Server.GetExistingSystemManaged<PrefabCollectionSystem>();
 
-    public static ServerGameManager ServerGameManager =>
-        Server.GetExistingSystemManaged<ServerScriptMapper>()._ServerGameManager;
-    
+    public static ActivateVBloodAbilitySystem ActivateVBloodAbilitySystem => Server.GetExistingSystemManaged<ActivateVBloodAbilitySystem>();
+
+    public static ServerGameManager ServerGameManager => Server.GetExistingSystemManaged<ServerScriptMapper>()._ServerGameManager;
+
     public static DebugEventsSystem DebugEventsSystem => Server.GetExistingSystemManaged<DebugEventsSystem>();
 
     public static EntityManager EntityManager => Server.EntityManager;
