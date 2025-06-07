c8c39252db6d1d9e2c29b1568be1329ededa7c6c
Crostomos <Crostomos@hotmail.com>
2025-06-07 03:23:27 +0200

    SpellModDb + AbilityToSpellModDb

---
+++ Commands/ManualConverters/SpellModManualConverter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using ArenaBuilds.Data.Db;
+using ArenaBuilds.Models.CommandArguments;
+using VampireCommandFramework;
+
+namespace ArenaBuilds.Commands.ManualConverters;
+
+public class SpellModManualConverter
+{
+    public static string GetSpellMod(ChatCommandContext ctx, AbilityModel ability, int spellModIndex)
+    {
+        if (AbilityToSpellModDb.AbilityToSpellMod.TryGetValue(ability.Name, out var spellMods))
+        {
+            var mod = spellMods.ElementAtOrDefault(spellModIndex - 1);
+            if (mod != null)
+            {
+                return mod;
+            }
+            
+            throw ctx.Error($"Unknown spell mod index <color=white>{spellModIndex}</color>.");
+        }
+
+        throw ctx.Error("Ability have no spell mods.");
+    }
+    
+    public static List<string> GetSpellModNameList(ChatCommandContext ctx, AbilityModel ability)
+    {
+        if (AbilityToSpellModDb.AbilityToSpellMod.TryGetValue(ability.Name, out var spellMods))
+        {
+            return spellMods
+                .Select(s => s.Replace("_", "")
+                    .Replace(ability.Name, "")
+                    .Replace("SpellMod", "")
+                    .Replace("Shared", ""))
+                .Select(s => Regex.Replace(s, @"([A-Z])", " $1").Trim())
+                .ToList();
+        }
+
+        throw ctx.Error($"Ability <color=white>{ability.Name}</color> have no spell mods.");
+    }
+}
--- Data/Db/SpellModDb.cs
+++ Data/Db/SpellModDb.cs
@@ -2,38 +2,94 @@
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using ArenaBuilds.Models.CommandArguments;
 using ArenaBuilds.Models.Interfaces;
 
 namespace ArenaBuilds.Data.Db;
 
 public class SpellModDb : IDatabase
 {
-    public static List<SpellModModel> Mods = [];
+    public static List<string> Mods = [];
+
+    // Shared
+    public const string ApplyFadingSnareLong = "SpellMod_Shared_ApplyFadingSnare_Long";
+    public const string ApplyFadingSnareMedium = "SpellMod_Shared_ApplyFadingSnare_Medium";
+    public const string ApplyFadingSnareShort = "SpellMod_Shared_ApplyFadingSnare_Short";
+    public const string DispellDebuffs = "SpellMod_Shared_DispellDebuffs";
+    public const string IncreaseMoveSpeedDuringChannelHigh = "SpellMod_Shared_IncreaseMoveSpeedDuringChannel_High";
+    public const string AddCharges = "SpellMod_Shared_AddCharges";
+    public const string ProjectileRangeAndVelocity = "SpellMod_Shared_Projectile_RangeAndVelocity";
+    public const string CastRate = "SpellMod_Shared_CastRate";
+    public const string CooldownMedium = "SpellMod_Shared_Cooldown_Medium";
+    public const string ProjectileIncreaseRangeMedium = "SpellMod_Shared_Projectile_IncreaseRange_Medium";
+    public const string IncreaseMoveSpeedDuringChannelLow = "SpellMod_Shared_IncreaseMoveSpeedDuringChannel_Low";
+    public const string KnockBackOnHitLight = "SpellMod_Shared_KnockBackOnHit_Light";
+    public const string KnockBackOnHitMedium = "SpellMod_Shared_KnockBackOnHit_Medium";
+    public const string TargetAoEIncreaseRangeMedium = "SpellMod_Shared_TargetAoE_IncreaseRange_Medium";
+    public const string TravelBuffIncreaseRangeMedium = "SpellMod_Shared_TravelBuff_IncreaseRange_Medium";
+    public const string MovementSpeedNormal = "SpellMod_Shared_MovementSpeed_Normal";
+
+    // Blood
+    public const string BloodConsumeLeechSelfHealBig = "SpellMod_Shared_Blood_ConsumeLeechSelfHeal_Big";
+
+    // Chaos
+    public const string ChaosConsumeIgniteAgonizingFlames = "SpellMod_Shared_Chaos_ConsumeIgniteAgonizingFlames";
+
+    // Frost
+    public const string FrostIncreaseFreezeWhenChill = "SpellMod_Shared_Frost_IncreaseFreezeWhenChill";
+    public const string FrostWeapon = "SpellMod_Shared_FrostWeapon";
+    public const string FrostSplinterNovaOnFrosty = "SpellMod_Shared_Frost_SplinterNovaOnFrosty";
+    public const string FrostShieldOnFrosty = "SpellMod_Shared_Frost_ShieldOnFrosty";
+    public const string FrostConsumeChillIntoFreezeRecast = "SpellMod_Shared_Frost_ConsumeChillIntoFreeze_Recast";
+
+    // Illusion
+    public const string IllusionConsumeWeakenSpawnWisp = "SpellMod_Shared_Illusion_ConsumeWeakenSpawnWisp";
+    public const string IllusionWeakenShieldOnAttack = "SpellMod_Shared_Illusion_WeakenShield_OnAttack";
+    public const string IllusionWeakenShield = "SpellMod_Shared_Illusion_WeakenShield";
+
+    // Storm
+    public const string StormConsumeStaticIntoStun = "SpellMod_Shared_Storm_ConsumeStaticIntoStun";
+    public const string StormConsumeStaticIntoWeaponCharge = "SpellMod_Shared_Storm_ConsumeStaticIntoWeaponCharge";
+    public const string StormConsumeStaticIntoStunExplode = "SpellMod_Shared_Storm_ConsumeStaticIntoStun_Explode";
+    public const string StormGrantWeaponCharge = "SpellMod_Shared_Storm_GrantWeaponCharge";
+
+    // Unholy
+    public const string UnholySkeletonBomb = "SpellMod_Shared_Unholy_SkeletonBomb";
+
+    // Veil
+    public const string VeilBonusDamageOnPrimary = "SpellMod_Shared_Veil_BonusDamageOnPrimary";
+    public const string VeilBuffAndIllusionDuration = "SpellMod_Shared_Veil_BuffAndIllusionDuration";
 
     public void Init()
     {
         foreach (var ability in AbilityDb.Abilities)
         {
-            AddToList(ability.Name);
+            AddToList(ability.Name, spellSchool: ability.SpellSchool);
         }
 
         AddToList("Shared");
     }
 
-    private static void AddToList(string name)
+    private static void AddToList(string name, string spellSchool = "")
     {
-        var regex = new Regex($"^SpellMod_{name}_[a-zA-Z]+$", RegexOptions.IgnoreCase);
+        List<string> regexList =
+        [
+            $"^SpellMod_{name}_[a-zA-Z]+$",
+            $"^SpellMod_{spellSchool}_{name}_[a-zA-Z]+$"
+        ];
 
-        var matchingProperties = typeof(Prefabs)
-            .GetFields(BindingFlags.Static | BindingFlags.Public)
-            .Where(prop => regex.IsMatch(prop.Name) && prop.Name.Count(c => c == '_') == 2)
-            .ToList();
-
-        foreach (var prop in matchingProperties)
+        foreach (var item in regexList)
         {
-            var modName = prop.Name.Split("_")[2];
-            Mods.Add(new SpellModModel(modName, prop.Name, []));
+            var regex = new Regex(item, RegexOptions.IgnoreCase);
+
+            var matchingProperties = typeof(Prefabs)
+                .GetFields(BindingFlags.Static | BindingFlags.Public)
+                .Where(prop => regex.IsMatch(prop.Name))
+                .ToList();
+
+            foreach (var prop in matchingProperties)
+            {
+                Mods.Add(prop.Name);
+            }
         }
     }
 }
