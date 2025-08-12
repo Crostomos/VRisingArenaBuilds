7a69e73d737e14b7bda0cf17b9b1302c5678820d
Crostomos <Crostomos@hotmail.com>
2025-08-13 00:30:55 +0200

    add player parameter on commands

--- Commands/BuildCommands.cs
+++ Commands/BuildCommands.cs
@@ -1,65 +1,69 @@
 ﻿using ArenaBuilds.Helpers;
+using ArenaBuilds.Models;
 using VampireCommandFramework;
 
 namespace ArenaBuilds.Commands;
 
 internal class BuildCommands
 {
-    [Command("give_build", "giveb", description: "Give a full build", adminOnly: false)]
-    public static void BuildCommand(ChatCommandContext ctx, string buildName)
+    [Command("give_build", "giveb", usage:"tank VampireName", description: "Give a full build", adminOnly: false)]
+    public static void BuildCommand(ChatCommandContext ctx, string buildName, PlayerData player = null)
     {
         if (BuildManager.Builds.Count == 0)
         {
             BuildManager.LoadData();
         }
 
-        if (BuildManager.Builds.TryGetValue(buildName, out var build))
+        if (!BuildManager.Builds.TryGetValue(buildName, out var build))
         {
-            if (build.Settings.ClearInventory)
-            {
-                InventoryHelper.ClearInventory(ctx.Event.SenderCharacterEntity);
-            }
-
-            WeaponHelper.GiveWeapons(ctx.User, ctx.Event.SenderCharacterEntity, build.Weapons);
-
-            if (build.Blood.GiveBloodPotion)
-            {
-                BloodHelper.GiveBloodPotion(
-                    ctx.Event.SenderCharacterEntity,
-                    build.Blood.PrimaryType,
-                    secondaryBloodType: build.Blood.SecondaryType,
-                    primaryQuality: build.Blood.PrimaryQuality,
-                    secondaryQuality: build.Blood.SecondaryQuality,
-                    secondaryBuffIndex: build.Blood.SecondaryBuffIndex
-                );
-            }
-
-            InventoryHelper.GiveItems(ctx.Event.SenderCharacterEntity, build.Items);
-
-            ArmorHelper.EquipArmors(ctx.Event.SenderCharacterEntity, build.Armors);
-
-            if (build.Blood.FillBloodPool)
-            {
-                BloodHelper.SetBlood(
-                    ctx.Event.SenderCharacterEntity,
-                    build.Blood.PrimaryType,
-                    secondaryBloodType: build.Blood.SecondaryType,
-                    primaryQuality: build.Blood.PrimaryQuality,
-                    secondaryQuality: build.Blood.SecondaryQuality,
-                    secondaryBuffIndex: build.Blood.SecondaryBuffIndex,
-                    amount: 100
-                );
-            }
-
-            AbilityHelper.EquipAbilities(ctx.Event.SenderCharacterEntity, ctx.User, build.Abilities);
-            AbilityHelper.EquipPassiveSpells(ctx.Event.SenderCharacterEntity, build.PassiveSpells);
-
-            ctx.Reply($"Equipped build <color=white>{buildName}</color>.");
+            throw ctx.Error($"Unknown build <color=white>{buildName}</color>.");
         }
-        else
+
+        if (player == null)
         {
-            ctx.Reply($"Unknown build <color=white>{buildName}</color>.");
+            player = new PlayerData(ctx.User, ctx.Event.SenderUserEntity);
         }
+
+        if (build.Settings.ClearInventory)
+        {
+            InventoryHelper.ClearInventory(player.CharacterEntity);
+        }
+
+        WeaponHelper.GiveWeapons(player.User, player.CharacterEntity, build.Weapons);
+
+        if (build.Blood.GiveBloodPotion)
+        {
+            BloodHelper.GiveBloodPotion(
+                player.CharacterEntity,
+                build.Blood.PrimaryType,
+                secondaryBloodType: build.Blood.SecondaryType,
+                primaryQuality: build.Blood.PrimaryQuality,
+                secondaryQuality: build.Blood.SecondaryQuality,
+                secondaryBuffIndex: build.Blood.SecondaryBuffIndex
+            );
+        }
+
+        InventoryHelper.GiveItems(player.CharacterEntity, build.Items);
+
+        ArmorHelper.EquipArmors(player.CharacterEntity, build.Armors);
+
+        if (build.Blood.FillBloodPool)
+        {
+            BloodHelper.SetBlood(
+                player.CharacterEntity,
+                build.Blood.PrimaryType,
+                secondaryBloodType: build.Blood.SecondaryType,
+                primaryQuality: build.Blood.PrimaryQuality,
+                secondaryQuality: build.Blood.SecondaryQuality,
+                secondaryBuffIndex: build.Blood.SecondaryBuffIndex,
+                amount: 100
+            );
+        }
+
+        AbilityHelper.EquipAbilities(player.CharacterEntity, player.User, build.Abilities);
+        AbilityHelper.EquipPassiveSpells(player.CharacterEntity, build.PassiveSpells);
+
+        ctx.Reply($"Equipped build <color=white>{buildName}</color> to <color=white>{player.CharacterName}</color>.");
     }
 
     [Command("list_build", "listb", description: "List available builds", adminOnly: false)]
@@ -81,19 +85,31 @@ internal class BuildCommands
     }
 
     [Command("clear_build", "clearb", description: "Clear current build", adminOnly: false)]
-    public static void ClearBuildCommand(ChatCommandContext ctx)
+    public static void ClearBuildCommand(ChatCommandContext ctx, PlayerData player = null)
     {
-        InventoryHelper.ClearInventory(ctx.Event.SenderCharacterEntity);
-        AbilityHelper.ClearPassiveSpells(ctx.Event.SenderCharacterEntity);
-        AbilityHelper.ClearAbilities(ctx.Event.SenderCharacterEntity);
+        if (player == null)
+        {
+            player = new PlayerData(ctx.User, ctx.Event.SenderUserEntity);
+        }
+
+        InventoryHelper.ClearInventory(player.CharacterEntity);
+        AbilityHelper.ClearPassiveSpells(player.CharacterEntity);
+        AbilityHelper.ClearAbilities(player.CharacterEntity);
 
         ctx.Reply("Build cleared.");
     }
 
     [Command("unlock_all", description: "Unlock All", adminOnly: true)]
-    public static void UnlockAllCommand(ChatCommandContext ctx)
+    public static void UnlockAllCommand(ChatCommandContext ctx, PlayerData player = null)
     {
-        PlayerHelper.UnlockAll(ctx.Event.SenderUserEntity, ctx.Event.SenderCharacterEntity);
-        ctx.Reply("Researches, VBloods and Achievements unlocked.");
+        if (player == null)
+        {
+            player = new PlayerData(ctx.User, ctx.Event.SenderUserEntity);
+        }
+
+        var userEntity = UtilsHelper.GetUserEntity(player.CharacterEntity);
+
+        PlayerHelper.UnlockAll(userEntity, player.CharacterEntity);
+        ctx.Reply($"Researches, VBloods and Achievements unlocked for <color=white>{player.CharacterName}</color>.");
     }
 }
