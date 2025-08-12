b2b36caca6e5caaba7064c05338329713d0782f2
Crostomos <Crostomos@hotmail.com>
2025-08-13 00:29:57 +0200

    add player converter

---
+++ Models/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using ProjectM.Network;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace ArenaBuilds.Models;
+
+internal class PlayerData(User user = default, Entity userEntity = default)
+{
+    public User User { get; set; } = user;
+
+    public Entity UserEntity { get; set; } = userEntity;
+
+    public FixedString64Bytes CharacterName => User.CharacterName;
+
+    public Entity CharacterEntity => User.LocalCharacter._Entity;
+}
