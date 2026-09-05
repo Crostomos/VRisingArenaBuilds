9d0606b82ee1425161535d838d805cb740874ff7
Crostomos <Crostomos@hotmail.com>
2026-09-05 21:10:15 +0200

    add fill_blood command

---
+++ Commands/BloodCommands.cs
@@ -0,0 +1,42 @@
+﻿using ArenaBuilds.Data.Db;
+using ArenaBuilds.Extensions;
+using ArenaBuilds.Helpers;
+using ArenaBuilds.Models;
+using ArenaBuilds.Models.CommandArguments;
+using VampireCommandFramework;
+
+namespace ArenaBuilds.Commands;
+
+internal class BloodCommands
+{
+    [Command("fill_blood", "fillb", "Draculin Scholar 1 VampireName", "Fill your blood", adminOnly: false)]
+    public static void GiveBloodCommand(
+        ChatCommandContext ctx,
+        BloodModel bloodTypePrimary,
+        BloodModel bloodTypeSecondary = null,
+        int secondaryBuffIndex = 0,
+        PlayerData player = null)
+    {
+        player ??= new PlayerData(ctx.User, ctx.Event.SenderUserEntity);
+
+        BloodHelper.SetBlood(
+            player.CharacterEntity,
+            bloodTypePrimary.PrefabName,
+            bloodTypeSecondary?.PrefabName ?? "",
+            100,
+            100,
+            secondaryBuffIndex
+        );
+
+        var bloodTypeString = bloodTypePrimary.Name;
+        if (bloodTypeSecondary != null) bloodTypeString += $"/{bloodTypeSecondary.Name}";
+        ctx.Reply(
+            $"Fill blood of <color=white>{player.CharacterName}</color> with <color=white>{bloodTypeString}</color>.");
+    }
+
+    [Command("list_blood", "listbl", description: "List bloods", adminOnly: false)]
+    public static void ListBloodsCommand(ChatCommandContext ctx)
+    {
+        ctx.Reply($"Bloods :\n{BloodDb.Bloods.ToFormattedList()}");
+    }
+}
---
+++ Commands/Converters/BloodConverter.cs
@@ -0,0 +1,19 @@
+﻿using ArenaBuilds.Data.Db;
+using ArenaBuilds.Extensions;
+using ArenaBuilds.Models.CommandArguments;
+using VampireCommandFramework;
+
+namespace ArenaBuilds.Commands.Converters;
+
+internal class BloodConverter : CommandArgumentConverter<BloodModel>
+{
+    public override BloodModel Parse(ICommandContext ctx, string input)
+    {
+        var match =
+            BloodDb.Bloods.EqualsCommandArgument(input) as BloodModel ??
+            BloodDb.Bloods.SearchCommandArgument(input) as BloodModel ??
+            throw ctx.Error($"Unknown blood type <color=white>{input}</color>.");
+
+        return match;
+    }
+}
